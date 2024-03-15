aaf231dac812997ab09c0cfde146ac879fb59f59
meuthak <marco.kaelin1@edu.tbz.ch>
2024-03-15 18:47:54 +0100

    msvc safety push

---
+++ MSVC/dev_env_setup/test/create_test_data.py
@@ -0,0 +1,38 @@
+from app.extensions import db
+from app.models.student import StudentsModel
+from app.models.course import CoursesModel
+
+# Hilfsfunktion (Testdaten erstellen, Tabellen erstellen)
+def create_test_data():
+    db.drop_all() # dieser Befehl löscht alle vorhandenen Datenbankeintraege und Tabellen
+    db.create_all()
+
+    # Beispieldaten
+    students = [
+        {'name': 'Freda Kids', 'level': 'HF'},
+        {'name': 'Sam Sung', 'level': 'HF'},
+        {'name': 'Chris P. Bacon', 'level': 'AP'},
+        {'name': 'Saad Maan', 'level': 'PE'}
+    ]
+    for student_data in students:
+        student = StudentsModel(**student_data)
+        db.session.add(student)
+
+    # create an additional student and safe ref for later
+    student_ref = StudentsModel(name='Mega Tron', level='HF')
+    db.session.add(student_ref)
+
+    courses = [
+        {'topic': 'M231 Security'},
+        {'topic': 'M254 BPMN'}
+    ]
+    for course_data in courses:
+        course = CoursesModel(**course_data)
+        db.session.add(course)
+
+    # create an additional course and safe ref for later
+    course_ref = CoursesModel(topic='M347 Kubernetes')
+    db.session.add(course_ref)
+    student_ref.courses.append(course_ref)
+
+    db.session.commit()
---
+++ MSVC/dev_env_setup/test/test_student.py
@@ -0,0 +1,12 @@
+from test import client
+
+def test_get_students(client):
+    response = client.get("/students/")
+    assert response.json[4]['name'] == 'Mega Tron'
+
+def test_post_student(client):
+    response = client.post("/students/create", json={
+            'name': 'Nina Hagen', 'level': 'AP'
+        })
+    assert response.status_code == 201
+
